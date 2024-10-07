60b8f7f5e93021b9d0e9ca96b8b9cf25583715d5
Othmane kun <156709799+Othmanekhomri@users.noreply.github.com>
2024-10-07 21:12:13 +0100

    Update code.js

---
+++ code.js
@@ -0,0 +1,17 @@
+let burger = document.getElementById('burger')
+let sidebar = document.getElementById('sidebar')
+let close_btn = document.getElementById('close_btn')
+
+
+
+burger.onclick=function(){
+   
+        sidebar.style.display="block"
+        
+    
+}
+
+close_btn.onclick=function(){
+        sidebar.style.display="none"
+        
+}
